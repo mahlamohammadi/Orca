0cf74048407964c4629783902f9fdac9f6e32390
mahlamohammadi <mahla.mohammadii99@gmail.com>
2021-06-05 15:40:22 +0430

    end splash and ui of login

---
+++ app/src/main/java/com/example/orca/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.example.orca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               Intent i = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
