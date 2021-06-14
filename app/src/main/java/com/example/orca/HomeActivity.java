27bcb0aaf08b6e1b8f40860c6cf564b44531cd61
mahlamohammadi <mahla.mohammadii99@gmail.com>
2021-06-14 15:15:27 +0430

    fragment manager

--- app/src/main/java/com/example/orca/HomeActivity.java
+++ app/src/main/java/com/example/orca/HomeActivity.java
@@ -1,32 +1,73 @@
 package com.example.orca;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.orca.databinding.ActivityHomeBinding;
 import com.example.orca.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+import me.ibrahimsn.lib.OnItemSelectedListener;
+
 public class HomeActivity extends AppCompatActivity {
     ActivityHomeBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
 
-        ArrayList<CategoryModel> categories=new ArrayList<>();
+        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content,new homeFragment());
+        transaction.commit();
+
+        binding.bottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+
+                switch (i)
+                {
+                    case 0:
+                        transaction.replace(R.id.content,new costFragment());
+                        transaction.commit();
+                        break;
+
+                    case 1:
+                        transaction.replace(R.id.content,new incomeFragment());
+                        transaction.commit();
+                        break;
+
+                    case 2:
+                        Toast.makeText(HomeActivity.this,"this is 2 Fragment",Toast.LENGTH_SHORT).show();
+
+                        break;
 
 
+                    case  3:
+                        transaction.replace(R.id.content,new homeFragment());
+                        transaction.commit();
+                        break;
 
-        CategoryAdapter adapter =new CategoryAdapter(this,categories);
-        binding.categoryList.setLayoutManager(new GridLayoutManager(this,1));
-        binding.categoryList.setAdapter(adapter);
 
+                }
+                return false;
+            }
+        });
 
     }
 }
